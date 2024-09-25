361657616e7b3208f8fcd68a6ec6588aecd1461b
JuniorNivellePXL <Junior.nivelle@student.pxl.be>
2024-09-25 10:45:14 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,28 @@
+alert("Hello");
+
+const h1Element = document.querySelector("h1");
+h1Element.innerText = naam;
+
+
+const getal = 3.2; // number
+const naam = 'Junior'; // string
+const WaarOFnietWaar = true; // of false // Boolean
+let voorbeeld; // undefined
+voorbeeld = null; // null
+const geenGetal = NaN; // NaN - Not a number
+
+let naam;
+naam = prompt("Wat is jouw naam?")
+
+/*
+
+let voorbeeld; // decraleren = let
+voorbeeld = 13
+
+const voorbeeldt = 13; // toekennen = const
+
+const een = 1;
+const twee = 2;
+const drie = 3;
+
+ */
